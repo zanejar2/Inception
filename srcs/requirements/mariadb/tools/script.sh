82939bb8ca4a84a008756ec0c310239ed97a2929
zanejar2 <zanejar@student.1337.ma>
2023-11-26 14:35:52 -0500

    almost done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 service mariadb start
-mariadb -u root -e "CREATE DATABASE rmdb ;"
-mariadb -u root -e "CREATE USER 'zanejar'@'%' IDENTIFIED BY 'prochazka' ;" 
-mariadb -u root -e "REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator' ;"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON rickdb.* TO 'zanejar'@'%' ;"
+mariadb -u root -e "CREATE DATABASE ${MARIADB_NAME} ;"
+mariadb -u root -e "CREATE USER '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PW}' ;" 
+mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MDB_USER}'@'%' ;"
 mariadb -u root -e "FLUSH PRIVILEGES;"
+service mariadb stop
+mariadbd
